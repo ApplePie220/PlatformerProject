ae2e81f2c6672acb87b52dc6a27e0b073c74fba1
CyberKilljoy <darkmilk85@gmail.com>
2022-05-05 15:16:29 +0300

    создан внешний мир и добавлены данные для следующих уровней

--- code/game_data.py
+++ code/game_data.py
@@ -8,4 +8,21 @@ level_0 = {
     'constraints': 'levels/0/level_0._constraints.csv',
     'player': 'levels/0/level_0._player.csv',
     'grass': 'levels/0/level_0._grass.csv',
+    'unlock': 1,
+    'content': 'this is level 0',
+    'node_pos': (110, 400)
+}
+level_1 = {'unlock': 2, 'content': 'this is level 1', 'node_pos': (300, 220)}
+level_2 = {'unlock': 3, 'content': 'this is level 2', 'node_pos': (480, 610)}
+level_3 = {'unlock': 4, 'content': 'this is level 3', 'node_pos': (610, 350)}
+level_4 = {'unlock': 5, 'content': 'this is level 4', 'node_pos': (880, 210)}
+level_5 = {'unlock': 5, 'content': 'this is level 5', 'node_pos': (1050, 400)}
+
+levels = {
+    0: level_0,
+    1: level_1,
+    2: level_2,
+    3: level_3,
+    4: level_4,
+    5: level_5
 }
--- code/main.py
+++ code/main.py
@@ -4,11 +4,24 @@ import sys
 from settings import *
 from level import Level
 from game_data import level_0
+from overworld import Overworld
+
+
+# класс для логи запуска внешнего мира
+class Game:
+    def __init__(self):
+        self.max_level = 3
+        self.overworld = Overworld(0, self.max_level, screen)
+
+    def run(self):
+        self.overworld.run()
+
 
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 level = Level(level_0, screen)
+game = Game()
 
 while True:
     for event in pygame.event.get():
@@ -17,7 +30,8 @@ while True:
             sys.exit()
 
     screen.fill('grey')
-    level.run()
+    game.run()
+    # level.run()
 
     pygame.display.set_caption("Приключения Тимоти Шаурмы")
     pygame.display.set_icon(pygame.image.load('graphics/icon/witch.gif'))
